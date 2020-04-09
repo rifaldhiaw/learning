360829d96d95839fc16753ed25e75715a4ac12f3
Rifaldhi AW <rifaldhiaw@ruangguru.com>
2020-04-10 01:04:08 +0700

    add "js syntax" content

---
+++ Javascript/syntax/main.js
@@ -0,0 +1,157 @@
+//
+// ─── COMMENTS: ──────────────────────────────────────────────────────────────────
+//
+// single line
+/*
+multi
+line
+*/
+
+// ─── DECLARING VARIABLES: ───────────────────────────────────────────────────────
+
+// var nama;
+// let nama = "user a";
+// const nama = "orang lain";
+
+//
+// ─── LOGGING TO THE CONSOLE: ────────────────────────────────────────────────────
+//
+// let x = "logging there";
+// console.log(x);
+// console.warn(x);
+// console.error(x);
+
+//
+// ─── TYPES OF VALUES: ───────────────────────────────────────────────────────────
+//
+// PRIMITIVE:
+// ----------
+// booleans
+// let x = false;
+// console.log(x)
+// console.log(typeof x)
+
+// numbers
+// let x = 10;
+// console.log(x)
+// console.log(typeof x)
+
+// strings
+// let x = 'a';
+// console.log(x);
+// console.log(typeof x);
+
+// undefined
+// let x;
+// console.log(x);
+// console.log(typeof x);
+
+// null
+// let x = null;
+// console.log(x);
+// console.log(typeof x);
+
+// symbols
+// bigInts -> new
+//
+// NON PRIMITIVE:
+// ---------
+// function
+// object
+
+//
+// ─── OPERATORS: ─────────────────────────────────────────────────────────────────
+//
+// booleans
+// let x = true && true;
+// let y = false || true;
+// console.log(y)
+
+// numbers
+// let x = 1 * 2;
+// let y = 4 / 2;
+// let z = 4 + 2;
+// let a = 4 - 2;
+// console.log(x, y, z, a)
+
+// strings
+// let x = "nama" + " saya";
+// console.log(x);
+
+// Comparison
+// let x = 1 > 2;
+// let y = 1 < 2;
+// let z = 1 >= 2;
+// let a = 1 <= 2;
+// let b = 1 === 2;
+// let c = 1 !== 2;
+// console.log(x, y, z, a, b, c);
+
+//
+// ─── CONTROL FLOW STATEMENTS: ───────────────────────────────────────────────────
+//
+// let x = 2;
+
+// if (x < 2) {
+//     console.log("< 2")
+// }
+
+// if (x < 2) {
+//     console.log("< 2")
+// } else {
+//     console.log("lain")
+// }
+
+// if (x < 2) {
+//     console.log("< 2")
+// } else if (x > 2) {
+//     console.log("> 2")
+// } 
+
+// if (x < 2) {
+//     console.log("< 2")
+// } else if (x > 2) {
+//     console.log("> 2")
+// } else {
+//     console.log("lain")
+// }
+
+// switch
+
+//
+// ─── ORDINARY FUNCTION DECLARATIONS: ────────────────────────────────────────────
+//
+// let addVar = function add(a, b) {
+//     return a + b;
+// }
+// console.log(addVar(2, 3));
+
+//
+// ─── ARROW FUNCTION EXPRESSIONS ─────────────────────────────────────────────────
+//
+// let add = (a, b) => a + b;
+// console.log(add(3, 10))
+// console.log(typeof add)
+
+//
+// ─── OBJECTS: ───────────────────────────────────────────────────────────────────
+//
+// let person = {
+//     name: "orang x",
+//     age: 18,
+// };
+// console.log(typeof person.name);
+
+//
+// ─── ARRAY: ─────────────────────────────────────────────────────────────────────
+//
+// let arr = [10, "orang", false];
+// let x = arr[2];
+// console.log(x);
+// console.log(typeof console);
+
+//
+// ─── MORE DATA STRUCTURES ───────────────────────────────────────────────────────
+//
+// - MAP
+// - SET
